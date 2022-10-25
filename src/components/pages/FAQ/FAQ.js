13e0cddff2a032cad1ddf852932f9531241790c6
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-10-25 15:57:19 +0600

    faq section design done

--- src/components/pages/FAQ/FAQ.js
+++ src/components/pages/FAQ/FAQ.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const FAQ = () => {
     return (
-        <div>
-            
+        <div className='w-[80%] h-96 mx-auto'>
+            <div className="collapse w-full">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-slate-700 text-primary-content peer-checked:bg-slate-600 peer-checked:text-secondary-content">
+                    Click me to show/hide content
+                </div>
+                <div className="collapse-content bg-slate-700 text-primary-content peer-checked:bg-slate-600 peer-checked:text-secondary-content">
+                    <p>hello</p>
+                </div>
+            </div>
         </div>
     );
 };
