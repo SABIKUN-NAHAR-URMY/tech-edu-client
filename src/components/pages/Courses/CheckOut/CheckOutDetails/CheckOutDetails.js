26e67de06ffabfe562b86ab8764e235cfe6010fc
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-10-27 19:13:21 +0600

    readme file create

--- src/components/pages/Courses/CheckOut/CheckOutDetails/CheckOutDetails.js
+++ src/components/pages/Courses/CheckOut/CheckOutDetails/CheckOutDetails.js
@@ -8,7 +8,7 @@ const CheckOutDetails = ({ ci }) => {
             <div className='text-2xl font-bold text-center bg-slate-800 p-8 text-slate-50'>{name}</div>
             {
                 courses.map(course => {
-                    return <div className='m-7 border-2 p-3 text-xl font-semibold rounded-lg'>
+                    return <div key={course.id} className='m-7 border-2 p-3 text-xl font-semibold rounded-lg'>
                         <p>Course Name: {course.title}</p>
                         <p>Author Name: {course.author.name}</p>
                         <p>Price: {course.author.price}</p>
